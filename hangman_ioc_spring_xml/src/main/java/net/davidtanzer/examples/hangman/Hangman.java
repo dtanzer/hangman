382eeab29b54d1b932311532bca635cbf0a26e27
David Tanzer <david@davidtanzer.net>
2015-01-05 11:15:30 +0100

    Refactored ioc_spring_xml to use XML config

--- hangman_ioc_spring_xml/src/main/java/net/davidtanzer/examples/hangman/Hangman.java
+++ hangman_ioc_spring_xml/src/main/java/net/davidtanzer/examples/hangman/Hangman.java
@@ -4,13 +4,13 @@ import net.davidtanzer.examples.hangman.dictionary.OneWordDictionary;
 import net.davidtanzer.examples.hangman.output.CommandLineOutput;
 import net.davidtanzer.examples.hangman.player.StrategicPlayer;
 import net.davidtanzer.examples.hangman.player.strategy.LetterFrequencyBasedStrategy;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Random;
 
 public class Hangman {
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(HangmanConfiguration.class);
+		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("spring-config.xml");
 		Game game = appContext.getBean(Game.class);
 		game.start();
 	}
